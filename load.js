2c67c78bdfb8e1da07a22da156132aaa4a356cd6
BrunoRNS <brunorns05@outlook.com>
2025-04-08 21:11:22 -0300

    Update load.js

--- load.js
+++ load.js
@@ -1,9 +1,4 @@
 window.onload = function() {
-      const themeToggle = document.getElementById("themeToggle");
-      const learnMoreBtn = document.getElementById("learnMoreBtn");
-      const homeLink = document.getElementById("goHome");
-      const aboutLink = document.getElementById("goAbout");
-      const toTipsBtn = document.getElementById("toTips");
 
       const homeSection = document.getElementById("home");
       const sSection = document.getElementById("selectable");
